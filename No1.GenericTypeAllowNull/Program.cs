a59943aa853ba564cb944bbd72cc376146ad3c8e
dimohy <58620778+dimohy@users.noreply.github.com>
2022-02-10 07:26:30 +0900

    초기 커밋.

---
+++ No1.GenericTypeAllowNull/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Runtime.CompilerServices;
+
+var i = new TestClass();
+
+Print(i.Add(5, 4));
+Print(i.Add(5, null));
+Print(i.Add("A", "B"));
+
+void Print(object? value, [CallerArgumentExpression("value")] string? argumentExpression = null)
+{
+    Console.WriteLine($"{argumentExpression}: {value}");
+}
+
+interface IAddable<T>
+{
+    T? Add(T? a, T? b);
+}
+
+
+class TestClass : IAddable<int>, IAddable<int?>, IAddable<string>
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int? Add(int? a, int? b)
+    {
+        if (a is null || b is null)
+            return null;
+
+        return a + b;
+    }
+
+    public string? Add(string? a, string? b)
+    {
+        if (a is null || b is null)
+            return null;
+
+        return a + b;
+    }
+}
