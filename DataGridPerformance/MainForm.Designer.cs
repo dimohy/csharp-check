15b549c162253c19eeaf24edacfc5deed9ce0aac
dimohy <58620778+dimohy@users.noreply.github.com>
2022-02-25 00:09:35 +0900

    6.DataGridPerformance

---
+++ DataGridPerformance/MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DataGridPerformance
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.startSimulationButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.intervalNumberic = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.callCountNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.itemsGridView = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.intervalNumberic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.callCountNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.startSimulationButton);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.intervalNumberic);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.callCountNumeric);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(825, 36);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // startSimulationButton
+            // 
+            this.startSimulationButton.Location = new System.Drawing.Point(6, 6);
+            this.startSimulationButton.Name = "startSimulationButton";
+            this.startSimulationButton.Size = new System.Drawing.Size(75, 23);
+            this.startSimulationButton.TabIndex = 0;
+            this.startSimulationButton.Text = "시작";
+            this.startSimulationButton.UseVisualStyleBackColor = true;
+            this.startSimulationButton.Click += new System.EventHandler(this.startSimulationButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(87, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 23);
+            this.label3.TabIndex = 6;
+            // 
+            // intervalNumberic
+            // 
+            this.intervalNumberic.Location = new System.Drawing.Point(249, 6);
+            this.intervalNumberic.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.intervalNumberic.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.intervalNumberic.Name = "intervalNumberic";
+            this.intervalNumberic.Size = new System.Drawing.Size(64, 23);
+            this.intervalNumberic.TabIndex = 1;
+            this.intervalNumberic.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(319, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ms 간격";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // callCountNumeric
+            // 
+            this.callCountNumeric.Location = new System.Drawing.Point(377, 6);
+            this.callCountNumeric.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.callCountNumeric.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.callCountNumeric.Name = "callCountNumeric";
+            this.callCountNumeric.Size = new System.Drawing.Size(64, 23);
+            this.callCountNumeric.TabIndex = 3;
+            this.callCountNumeric.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Location = new System.Drawing.Point(447, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "개 발생";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // itemsGridView
+            // 
+            this.itemsGridView.AllowUserToAddRows = false;
+            this.itemsGridView.AllowUserToDeleteRows = false;
+            this.itemsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.itemsGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.itemsGridView.Location = new System.Drawing.Point(0, 36);
+            this.itemsGridView.Name = "itemsGridView";
+            this.itemsGridView.ReadOnly = true;
+            this.itemsGridView.RowHeadersWidth = 16;
+            this.itemsGridView.RowTemplate.Height = 25;
+            this.itemsGridView.Size = new System.Drawing.Size(825, 392);
+            this.itemsGridView.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(825, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 450);
+            this.Controls.Add(this.itemsGridView);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "실시간 시세 표시 (시뮬레이션)";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.intervalNumberic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.callCountNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemsGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button startSimulationButton;
+        private NumericUpDown intervalNumberic;
+        private DataGridView itemsGridView;
+        private StatusStrip statusStrip1;
+        private Label label3;
+        private Label label1;
+        private NumericUpDown callCountNumeric;
+        private Label label2;
+    }
+}
---
+++ DataGridPerformance/MainForm.cs
@@ -0,0 +1,153 @@
+using System.ComponentModel;
+
+namespace DataGridPerformance;
+
+public partial class MainForm : Form
+{
+    private CancellationTokenSource? _simulationCts;
+
+
+    public BindingList<종목정보> 종목목록 { get; } = new BindingList<종목정보>()
+    {
+        new(1, "A", 500, 0, 450, 550),
+        new(2, "B", 500, 0, 450, 550),
+        new(3, "C", 500, 0, 450, 550),
+        new(4, "D", 500, 0, 450, 550),
+        new(5, "E", 500, 0, 450, 550),
+        new(6, "F", 500, 0, 450, 550),
+        new(7, "G", 500, 0, 450, 550),
+        new(8, "H", 500, 0, 450, 550),
+        new(9, "I", 500, 0, 450, 550),
+        new(10, "J", 500, 0, 450, 550),
+    };
+
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+
+
+        itemsGridView.AutoGenerateColumns = true;
+        itemsGridView.DataSource = 종목목록;
+
+        종목목록.ListChanged += async (s, e) =>
+        {
+            if (e.ListChangedType is ListChangedType.ItemChanged)
+            {
+                var propertyName = e.PropertyDescriptor!.Name;
+
+                var rowIndex = -1;
+                int columnIndex;
+                for (columnIndex = 0; columnIndex < itemsGridView.Columns.Count; columnIndex++)
+                {
+                    var column = itemsGridView.Columns[columnIndex];
+                    if (column.DataPropertyName == propertyName)
+                    {
+                        rowIndex = e.NewIndex;
+                        break;
+                    }
+                }
+
+                itemsGridView.Rows[rowIndex].Cells[columnIndex].Style.BackColor = Color.Red;
+                await Task.Delay(1000);
+                itemsGridView.Rows[rowIndex].Cells[columnIndex].Style.BackColor = default;
+            };
+        };
+    }
+
+    private async void startSimulationButton_Click(object sender, EventArgs e)
+    {
+        if (_simulationCts is not null && _simulationCts?.IsCancellationRequested is false)
+        {
+            _simulationCts?.Cancel();
+            return;
+        }
+
+        startSimulationButton.Text = "중지";
+
+        var interval = (int)intervalNumberic.Value;
+        var callCount = (int)callCountNumeric.Value;
+
+        _simulationCts = new CancellationTokenSource();
+        await Task.Run(async () =>
+        {
+            try
+            {
+                while (_simulationCts.Token.IsCancellationRequested is false)
+                {
+                    for (var i = 0; i < callCount; i++)
+                    {
+                        var 종목index = Random.Shared.Next(0, 종목목록.Count);
+                        var 종목 = 종목목록[종목index];
+                        var cmd = Random.Shared.Next(0, 6);
+                        // 매수호가 변경
+                        if (cmd is 0 or 1)
+                        {
+                            종목.매수호가 += cmd is 0 ? +5 : -5; ;
+                            종목.FireProeprtChanged(nameof(종목정보.매수호가));
+                        }
+                        // 매도호가 변경
+                        else if (cmd is 2 or 3)
+                        {
+                            종목.매도호가 += cmd is 2 ? +5 : -5;
+                            종목.FireProeprtChanged(nameof(종목정보.매도호가));
+                        }
+                        // 매수 체결
+                        else if (cmd is 4)
+                        {
+                            종목.현재가 = 종목.매도호가;
+                            종목.체결건수++;
+                            종목.FireProeprtChanged(nameof(종목정보.현재가));
+                            종목.FireProeprtChanged(nameof(종목정보.체결건수));
+                        }
+                        // 매도 체결
+                        else if (cmd is 5)
+                        {
+                            종목.현재가 = 종목.매수호가;
+                            종목.체결건수++;
+                            종목.FireProeprtChanged(nameof(종목정보.현재가));
+                            종목.FireProeprtChanged(nameof(종목정보.체결건수));
+                        }
+                    }
+
+                    await Task.Delay(interval, _simulationCts.Token);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }, _simulationCts.Token);
+        startSimulationButton.Text = "시작";
+    }
+}
+
+public class 종목정보 : INotifyPropertyChanged
+{
+    public int 순번 { get; }
+    public string 종목 { get; }
+    public decimal 현재가 { get; set; }
+    public int 체결건수 { get; set; }
+    public decimal 매수호가 { get; set; }
+    public decimal 매도호가 { get; set; }
+
+    public 종목정보(int 순번, string 종목, decimal 현재가, int 체결건수, decimal 매수호가, decimal 매도호가)
+    {
+        this.순번 = 순번;
+        this.종목 = 종목;
+        this.현재가 = 현재가;
+        this.체결건수 = 체결건수;
+        this.매수호가 = 매수호가;
+        this.매도호가 = 매도호가;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void FireProeprtChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
