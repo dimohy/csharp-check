105a3bd074dda8ab10c463c5265b12cd823290c7
dimohy <58620778+dimohy@users.noreply.github.com>
2022-03-11 19:11:00 +0900

    No5.StringBuilderHelper

---
+++ No4.TaskCompletionSource/Program.cs
@@ -0,0 +1,9 @@
+﻿var task = Task.Run(async () =>
+{
+    await Task.Delay(30000);
+});
+
+Console.WriteLine("Start!");
+await task;
+Console.WriteLine("Stop!");
+
---
+++ No5.StringBuilderHelper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+
+var nums = new[] { 1, 2, 3, 4, 5 };
+
+var result =
+$@"nums {{
+    {nums.Foreach((s, n) => s.Append($"{n}, "))}
+}}";
+
+Console.WriteLine(result);
+
+
+public static class StringBuilderExtension
+{
+    public static StringBuilder Foreach<T>(this IEnumerable<T> enumerable, Action<StringBuilder, T> funcCallback)
+    {
+        var result = new StringBuilder();
+
+        foreach (var item in enumerable)
+        {
+            funcCallback?.Invoke(result, item);
+        }
+
+        return result;
+    }    
+}
