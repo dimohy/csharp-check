4b553a66b6b6f0b160858aa3606bffc50e439a53
dimohy <58620778+dimohy@users.noreply.github.com>
2022-04-22 17:43:19 +0900

    WpfSelectedTwoFilter

---
+++ No9.CSharpCodeRunner/Client/Pages/Index.razor.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using MudBlazor;
+using Microsoft.AspNetCore.Components;
+using System.Text;
+using System.Xml.Linq;
+
+namespace No9.CSharpCodeRunner.Client.Pages
+{
+    public partial class Index
+    {
+        private static IEnumerable<string> usings = new[]
+        {
+                "System",
+                "System.IO",
+                "System.Collections.Generic",
+                "System.Console",
+                "System.Diagnostics",
+                "System.Dynamic",
+                "System.Linq",
+                "System.Linq.Expressions",
+                "System.Net.Http",
+                "System.Text",
+                "System.Threading.Tasks"
+        };
+
+        private IEnumerable<MetadataReference>? _references;
+        private CSharpCompilation? _previousCompilation;
+
+        private string sourceText = "";
+        private string? outputText;
+
+        private void RunCSharpSource()
+        {
+            outputText = RunCSharpSource(sourceText);
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            //var refs = AppDomain.CurrentDomain.GetAssemblies();
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(navigationManager.BaseUri)
+            };
+
+            var references = new List<MetadataReference>();
+
+//foreach (var reference in refs.Where(x => !x.IsDynamic && !string.IsNullOrWhiteSpace(x.Modules.First().Name)))
+//{
+//    try
+//    {
+//        var name = reference.Modules.First().Name;
+//        var stream = await client.GetStreamAsync($"_framework/{name}");
+//        references.Add(MetadataReference.CreateFromStream(stream));
+//    }
+//    catch (Exception)
+//    {
+//    }
+//}
+            foreach (var name in usings)
+            {
+                try
+                {
+                    var stream = await client.GetStreamAsync($"_framework/{name}.dll");
+                    references.Add(MetadataReference.CreateFromStream(stream));
+                }
+                catch
+                {
+                }
+            }
+
+            _references = references;
+        }
+
+        private string? RunCSharpSource(string source)
+        {
+            var scriptCompilation = CSharpCompilation.CreateScriptCompilation(
+                Path.GetRandomFileName(),
+                CSharpSyntaxTree.ParseText(source,
+                CSharpParseOptions.Default.WithKind(SourceCodeKind.Script).WithLanguageVersion(LanguageVersion.Preview)),
+                _references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, usings: usings),
+                _previousCompilation
+            );
+
+            using var ms = new MemoryStream();
+            var emitResult = scriptCompilation.Emit(ms);
+            if (emitResult.Success is true)
+            {
+                _previousCompilation = scriptCompilation;
+                return "";
+            }
+
+            var sb = new StringBuilder();
+            foreach (var diagnostic in emitResult.Diagnostics)
+            {
+                sb.AppendLine(diagnostic.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ WpfSelectedTwoFilter/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSelectedTwoFilter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public FilterInfo RootFilter { get; } = new FilterInfo("ROOT", new List<FilterInfo> {
+            new FilterInfo("depth1_1", new List<FilterInfo>
+            {
+                new FilterInfo("depth2_11", Enumerable.Empty<FilterInfo>()),
+                new FilterInfo("depth2_12", Enumerable.Empty<FilterInfo>()),
+                new FilterInfo("depth2_13", Enumerable.Empty<FilterInfo>())
+            }),
+            new FilterInfo("depth1_2", Enumerable.Empty<FilterInfo>()),
+            new FilterInfo("depth1_3", Enumerable.Empty<FilterInfo>()),
+        });
+
+        public SelectedFilter SelectedFilter { get; } = new SelectedFilter();
+
+        public MainWindow()
+        {
+            DataContext = this;
+            
+            InitializeComponent();
+        }
+    }
+
+    public class SelectedFilter : INotifyPropertyChanged
+    {
+        private FilterInfo? _depth1;
+        private FilterInfo? _depth2;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public FilterInfo? Depth1
+        {
+            get => _depth1;
+            set
+            {
+                if (_depth1 != value)
+                {
+                    _depth1 = value;
+                    OnPropertyChanged(nameof(Depth1));
+                }
+            }
+        }
+        public FilterInfo? Depth2
+        {
+            get => _depth2;
+            set
+            {
+                if (_depth2 != value)
+                {
+                    _depth2 = value;
+                    OnPropertyChanged(nameof(Depth2));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class FilterInfo
+    {
+        public string Text { get; }
+        public IEnumerable<FilterInfo> Children { get; }
+        
+        public FilterInfo(string text, IEnumerable<FilterInfo> children)
+        {
+            this.Text = text;
+            this.Children = children;
+        }
+    }
+}
