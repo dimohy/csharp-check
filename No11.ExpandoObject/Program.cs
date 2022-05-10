4b52c4b738ac0014f57b74926fdd34efdfa28e47
dimohy <58620778+dimohy@users.noreply.github.com>
2022-05-11 00:06:48 +0900

    No12.WpfBindingExpandoObject

---
+++ No11.ExpandoObject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Dynamic;
+
+dynamic p = new ExpandoObject();
+
+(p as INotifyPropertyChanged)!.PropertyChanged += (s, e) =>
+{
+    Console.WriteLine($"Changed Property : {e.PropertyName}");
+};
+
+// Console.WriteLine(p.Name); // 아직 Name 속성이 없으므로 예외
+
+p.Name = "David";
+p.Age = 30;
+
+IDictionary<string, object?> dic = p;
+
+dic["Sex"] = "Male";
+
+Console.WriteLine(p.Sex);
+
+foreach (var kv in p)
+{
+    Console.WriteLine(kv);
+}
---
+++ No12.WpfBindingExpandoObject/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace No12.WpfBindingExpandoObject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public dynamic UserInfo { get; } = new ExpandoObject();
+
+        public MainWindow()
+        {
+            DataContext = this;
+
+            InitializeComponent();
+
+            //UserInfo.Name = "John";
+        }
+    }
+}
