dd2e2db7da18efab2f034ad8f73541d281490c25
dimohy <58620778+dimohy@users.noreply.github.com>
2022-09-21 01:21:56 +0900

    No18.DefaultInterfaceMethods

---
+++ DotnetDevQnA/ConvertInvalidFilename.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+(char, char)[] _fileInvalidCharMap =
+{
+    ('\\', '＼'),
+    ('/', '／'),
+    (':', '：'),
+    ('*', '＊'),
+    ('?', '？'),
+    ('"', '＂'),
+    ('<', '＜'),
+    ('>', '＞'),
+    ('|', '｜')
+};
+
+
+//var filename = "A: B, [test] <123>.txt";
+var filename = "＼／：＊？＂＜＞｜.txt";
+var newFilename = GetConvertName(filename);
+Console.WriteLine(newFilename);
+newFilename = GetOriginalName(newFilename);
+Console.WriteLine(newFilename);
+Console.WriteLine(filename == newFilename);
+
+
+string GetOriginalName(string name)
+{
+    var result = new StringBuilder();
+
+    foreach (var c in name)
+    {
+        var index = Array.FindIndex(_fileInvalidCharMap, x => x.Item2 == c);
+        if (index < 0)
+        {
+            result.Append(c);
+            continue;
+        }
+
+        result.Append(_fileInvalidCharMap[index].Item1);
+    }
+
+    return result.ToString();
+}
+
+string GetConvertName(string name)
+{
+    var result = new StringBuilder();
+
+    foreach (var c in name)
+    {
+        var index = Array.FindIndex(_fileInvalidCharMap, x => x.Item1 == c);
+        if (index < 0)
+        {
+            result.Append(c);
+            continue;
+        }
+
+        result.Append(_fileInvalidCharMap[index].Item2);
+    }
+
+    return result.ToString();
+}
---
+++ No18.DefaultInterfaceMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+var e = new Enum();
+Test1(e);
+
+
+static void Test1<T>(IEnum<T> enums)
+{
+    Console.WriteLine($"Count: {enums.NewCount()}");
+
+    foreach (var n in enums)
+        Console.WriteLine(n);
+}
+
+
+interface IEnum<T> : IEnumerable<T>
+{
+
+    int NewCount()
+    {
+        var count = 0;
+        foreach (var i in this)
+            count++;
+        return count;
+    }
+}
+
+class Enum : IEnum<int>
+{
+    private readonly List<int> _list = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//    int IEnum<int>.NewCount() => _list.Count;
+
+    public IEnumerator<int> GetEnumerator() => _list.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => _list.GetEnumerator();
+}
