7e6fbba6e9b949e7ef3b0ec88f65eb7bdb347794
dimohy <58620778+dimohy@users.noreply.github.com>
2022-12-22 23:51:47 +0900

    QnA/WpfResize

---
+++ QnA/WpfResize/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfResize
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            var source = PresentationSource.FromVisual(this) as HwndSource;
+            source!.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_ENTERSIZEMOVE = 0x0231;
+            const int WM_EXITSIZEMOVE = 0x0232;
+
+            if (msg is WM_ENTERSIZEMOVE)
+            {
+                content.Width = content.ActualWidth;
+                content.Height = content.ActualHeight;
+                Debug.WriteLine("ENTER!");
+            }
+            else if (msg is WM_EXITSIZEMOVE)
+            {
+                content.Width = double.NaN;
+                content.Height = double.NaN;
+
+                Debug.WriteLine("EXIT!");
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
