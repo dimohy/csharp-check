471041f7d0126f5e7368e6f6dfd5270036c9d640
dimohy <58620778+dimohy@users.noreply.github.com>
2023-01-08 15:15:33 +0900

    N25.IsUseful

---
+++ DotnetDevWinFormsQnA/ShowDialogWhenLongWorkingForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotnetDevWinFormsQnA;
+
+public partial class ShowDialogWhenLongWorkingForm : Form
+{
+    public ShowDialogWhenLongWorkingForm()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        var waitForm = new WaitForm();
+        new Thread(() =>
+        {
+            BeginInvoke(() => waitForm.ShowDialog());
+            // 오래 걸리는 작업 시뮬레이션
+            Thread.Sleep(5000);
+            BeginInvoke(() => waitForm.Close());
+        }).Start();
+    }
+}
---
+++ No25.IsUseful/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = "15,2";
+
+// 1. 입력 값을 변수에 담는 일반적인 방법
+var items = input.Split(',');
+var (a, b) = (items[0], items[1]);
+
+// 2. LINQ를 쓸 수도 있음
+(a, b) = input.Split(',').Chunk(2).Select(x => (x[0], x[1])).First();
+
+// 3. is를 사용하면 비교문에서 사용할 수 있음
+var bResult = input.Split(',') is string[] items2 is true && items2[0] is "15" && items2[1] is "2";
+Console.WriteLine(bResult);
+
+Console.WriteLine($"a={a}, b={b}");
